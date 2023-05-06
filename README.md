208308e902d092f2c3300e560b1fc949601bdcf4
Matías Ramira <matias.ramira@smartway.com.ar>
2023-05-06 12:20:15 -0300

    Modal en caliz, ajustes menores

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Escena creada con AFRAME para aprender algunos conceptos basicos.
 
-La escena permite visualizar una imagen 360º de la capilla de Paraná, Entre Rios, tanto por fuera como por dentro.
-Al posarse en "Interior", se observa el interior de la capilla y al posarse en "Exterior" se observa la fachada por fuera.
+La escena permite visualizar una imagen 360º de la catedral de Paraná, Entre Rios, tanto por fuera como por dentro.
+Al posarse en "Interior", se observa el interior de la catedral y al posarse en "Exterior" se observa la fachada por fuera.
 
-En el interior de la capilla se añadió una imagen de un caliz con una animación.
+En el interior de la catedral se añadió una imagen de un caliz con una animación que, al darle click, mostrara un modal con una imagen de la catedral por dentro vista desde la puerta.
 
 https://ramira10.github.io/vr-scene2/
--- js/caliz.js
+++ js/caliz.js
@@ -1,16 +1,29 @@
 AFRAME.registerComponent('on-mouseenter', {
-    schema: {
-        scale: { default: '1.5 1.5 1.5', type: 'string' }
-    },
+    schema: {},
 
     init: function () {
         var data = this.data;
         var el = this.el;
 
         this.el.addEventListener('click', function () {
-            alert('Caliz')
+            var isVisible = el.getAttribute('visible');
+
+            // Verificar el valor de la propiedad "visible"
+            if (isVisible.toString() === 'true') {
+                var modalContent = `
+    <div style="text-align: center;">
+      <h1>Catedral de Nuestra Señora del Rosario (Paraná)</h1>
+      <img src="../assets/Interior.jpg" style="width: 100%;">
+    </div>
+  `
+                // Mostrar el modal informativo
+                Swal.fire({
+                    html: modalContent,
+                    showCloseButton: true,
+                    showConfirmButton: false,
+                });
+            }
         });
     }
 });
-
 
